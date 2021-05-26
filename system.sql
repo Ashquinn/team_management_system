bf6b87c98f401bc785f4d3536492e7e7f59b63ec
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-05-26 17:48:17 -0400

    system.sql file added. Syntax added to system.sql file to create systemDB database and create three tables; department, role, and employee.

---
+++ system.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS systemDB;
+
+CREATE DATABASE systemDB;
+
+USE systemDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
